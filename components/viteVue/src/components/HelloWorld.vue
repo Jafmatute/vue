de5117759f235073bc5e82a634d69f7a0f2b8993
Jafmatute <Jafmatute@gmail.com>
2021-06-25 00:48:19 -0600

    Passing parameters to components

--- components/viteVue/src/components/HelloWorld.vue
+++ components/viteVue/src/components/HelloWorld.vue
@@ -1,29 +1,22 @@
 <template>
   <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<!--<script setup>
+import { defineProps } from 'vue'
 
 defineProps({
   msg: String
 })
+</script>-->
 
-const state = reactive({ count: 0 })
+<script type="text/javascript">
+  export default {
+    // props: ['msg']
+    props: {
+      msg:String
+    }
+  }
 </script>
 
 <style scoped>
