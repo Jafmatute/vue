96365301df8d829b21c2e583fa265c3f4267f420
Jafmatute <Jafmatute@gmail.com>
2021-06-26 22:53:06 -0600

    dynamic components

--- components/viteVue/src/App.vue
+++ components/viteVue/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-<AppLayout>
-  <!-- Header -->
+<!-- <AppLayout>
+
   <template v-slot:header>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" @emitE="handleEmit" />
   </template>
-  <!-- Body -->
+
   <template v-slot:body>
     <Bottom>enviar</Bottom>
   </template>
 
-</AppLayout>
+</AppLayout> -->
+<keep-alive>
+  <component :is="currentComponent" ></component>
+</keep-alive>
+
+<button @click="currentComponent = 'Home'" type="button">Home</button>
+<button @click="currentComponent = 'About'" type="button">About</button>
+
 </template>
 
 <!--<script setup>
@@ -25,18 +32,24 @@ import AppLayout from './components/AppLayout.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import Bottom from './components/Bottom.vue'
 import AppLayout from './components/AppLayout.vue'
+
+//Components reutilizables
+import Home from './components/Home.vue'
+import About from './components/About.vue'
   export default {
 
     data(){
 
       return{
-
+        currentComponent:"home",
       }
     },
     components:{
       HelloWorld,
       Bottom,
-      AppLayout
+      AppLayout,
+      Home,
+      About,
     },
     methods:{
       handleEmit(e){
---
+++ components/viteVue/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="">
+    <h1>Home</h1>
+    <input type="text">
+  </div>
+</template>
+<script>
+export default {
+
+
+}
+</script>
